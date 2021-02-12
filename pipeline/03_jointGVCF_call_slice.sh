8d6b5660cd95c49a5c3d9b8bad438d227942a426
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-12 10:09:50 -0800

    update for multi-sub populations

--- pipeline/03_jointGVCF_call_slice.sh
+++ pipeline/03_jointGVCF_call_slice.sh
@@ -9,15 +9,15 @@ module load gatk/4
 module load java/13
 module load bcftools
 module load parallel
+module load yq
 
 source config.txt
 
 declare -x TEMPDIR=$TEMP/$USER/$$
 
-cleanup() { 
+cleanup() {
 	#echo "rm temp is: $TEMPDIR"
 	rm -rf $TEMPDIR
-	rmdir $TEMPDIR
 }
 
 # Set trap to ensure cleanupis stopped
@@ -36,19 +36,9 @@ fi
 if [ -f config.txt ]; then
 	source config.txt
 fi
-if [ -z $SLICEVCF ]; then
-	SLICEVCF=vcf_slice
-fi
-mkdir -p $SLICEVCF
-STEM=$SLICEVCF/$PREFIX.$N
-GENOVCFOUT=$STEM.all.vcf
-FILTERSNP=$STEM.SNP.filter.vcf
-FILTERINDEL=$STEM.INDEL.filter.vcf
-SELECTSNP=$STEM.SNP.selected.vcf
-SELECTINDEL=$STEM.INDEL.selected.vcf
 
 if [ ! -f $REFGENOME ]; then
-    module load samtools/1.9
+    module load samtools/1.11
     samtools faidx $REFGENOME
 fi
 NSTART=$(perl -e "printf('%d',1 + $GVCF_INTERVAL * ($N - 1))")
@@ -72,101 +62,121 @@ if [[ $(ls $GVCFFOLDER | grep -c -P "\.g.vcf$") -gt "0" ]]; then
     parallel -j $CPU tabix -f {} ::: $GVCFFOLDER/*.g.vcf.gz
 fi
 
-FILES=$(ls $GVCFFOLDER/*.g.vcf.gz | sort | perl -p -e 's/(\S+)\n/-V $1 /')
-INTERVALS=$(cut -f1 $REFGENOME.fai  | sed -n "${NSTART},${NEND}p" | perl -p -e 's/(\S+)\n/--intervals $1 /g')
-mkdir -p $TEMPDIR
-if [ ! -f $GENOVCFOUT.gz ]; then
-    if [ ! -f $GENOVCFOUT ]; then
-	DB=$TEMPDIR/${GVCFFOLDER}_slice_$N
-	rm -rf $DB
-	gatk  --java-options "-Xmx$MEM -Xms$MEM" GenomicsDBImport --consolidate --merge-input-intervals --genomicsdb-workspace-path $DB $FILES $INTERVALS --tmp-dir $TEMPDIR --reader-threads $CPU
-	#--reader-threads $CPU
-	#gatk  --java-options "-Xmx$MEM -Xms$MEM" GenomicsDBImport --genomicsdb-workspace-path $DB $FILES $INTERVALS  --reader-threads $CPU
-	time gatk GenotypeGVCFs --reference $REFGENOME --output $GENOVCFOUT -V gendb://$DB --tmp-dir $TEMPDIR
-	ls -l $TEMPDIR
-	rm -rf $DB
-    fi
-    if [ -f $GENOVCFOUT ]; then
-    	bgzip $GENOVCFOUT
-    	tabix $GENOVCFOUT.gz
-    fi
+if [[ -z $POPYAML || ! -s $POPYAML ]]; then
+	echo "Cannot find \$POPYAML variable - set in config.txt"
+	exit
 fi
-TYPE=SNP
-echo "VCF = $STEM.$TYPE.vcf.gz"
-if [[ ! -f $STEM.$TYPE.vcf.gz ]]; then
-    gatk SelectVariants \
-	-R $REFGENOME \
-	--variant $GENOVCFOUT.gz \
-	-O $STEM.$TYPE.vcf \
-	--restrict-alleles-to BIALLELIC \
-	--select-type-to-include $TYPE --create-output-variant-index false
-
-    bgzip $STEM.$TYPE.vcf
-    tabix $STEM.$TYPE.vcf.gz
+if [ -z $SLICEVCF ]; then
+	SLICEVCF=vcf_slice
 fi
+mkdir -p $SLICEVCF
+for POPNAME in $(yq eval '.Populations | keys' $POPYAML | perl -p -e 's/^\s*\-\s*//')
+do
+	FILES=$(yq eval '.Populations.'$POPNAME'[]' $POPYAML | perl -p -e "s/(\S+)/-V $GVCFFOLDER\/\$1.g.vcf.gz/g"  )
+	INTERVALS=$(cut -f1 $REFGENOME.fai  | sed -n "${NSTART},${NEND}p" | perl -p -e 's/(\S+)\n/--intervals $1 /g')
 
-if [[ ! -f $FILTERSNP.gz || $STEM.$TYPE.vcf.gz -nt $FILTERSNP.gz ]]; then
-    gatk VariantFiltration --output $FILTERSNP \
-	--variant $STEM.$TYPE.vcf.gz -R $REFGENOME \
-	--cluster-window-size 10  \
-	--filter-expression "QD < 2.0" --filter-name QualByDepth \
-	--filter-expression "MQ < 40.0" --filter-name MapQual \
-	--filter-expression "QUAL < 100" --filter-name QScore \
-	--filter-expression "SOR > 4.0" --filter-name StrandOddsRatio \
-	--filter-expression "FS > 60.0" --filter-name FisherStrandBias \
-	--missing-values-evaluate-as-failing --create-output-variant-index false
+	mkdir -p $SLICEVCF/$POPNAME
+	STEM=$SLICEVCF/$POPNAME/$PREFIX.$N
+	GENOVCFOUT=$STEM.all.vcf
+	FILTERSNP=$STEM.SNP.filter.vcf
+	FILTERINDEL=$STEM.INDEL.filter.vcf
+	SELECTSNP=$STEM.SNP.selected.vcf
+	SELECTINDEL=$STEM.INDEL.selected.vcf
+	echo "$STEM is stem; GENOVCFOUT=$STEM.all.vcf POPNAME=$POPNAME slice=$SLICEVCF"
+	mkdir -p $TEMPDIR
+	if [ ! -f $GENOVCFOUT.gz ]; then
+	    if [ ! -f $GENOVCFOUT ]; then
+		DB=$TEMPDIR/${GVCFFOLDER}_slice_$N
+		rm -rf $DB
+		gatk  --java-options "-Xmx$MEM -Xms$MEM" GenomicsDBImport --consolidate --merge-input-intervals --genomicsdb-workspace-path $DB $FILES $INTERVALS --tmp-dir $TEMPDIR --reader-threads $CPU
+		#--reader-threads $CPU
+		#gatk  --java-options "-Xmx$MEM -Xms$MEM" GenomicsDBImport --genomicsdb-workspace-path $DB $FILES $INTERVALS  --reader-threads $CPU
+		time gatk GenotypeGVCFs --reference $REFGENOME --output $GENOVCFOUT -V gendb://$DB --tmp-dir $TEMPDIR
+		ls -l $TEMPDIR
+		rm -rf $DB
+	    fi
+	    if [ -f $GENOVCFOUT ]; then
+	    	bgzip $GENOVCFOUT
+	    	tabix $GENOVCFOUT.gz
+	    fi
+	fi
+	TYPE=SNP
+	echo "VCF = $STEM.$TYPE.vcf.gz"
+	if [[ ! -f $STEM.$TYPE.vcf.gz ]]; then
+	    gatk SelectVariants \
+		-R $REFGENOME \
+		--variant $GENOVCFOUT.gz \
+		-O $STEM.$TYPE.vcf \
+		--restrict-alleles-to BIALLELIC \
+		--select-type-to-include $TYPE --create-output-variant-index false
 
-#	--filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
-#	--filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
+	    bgzip $STEM.$TYPE.vcf
+	    tabix $STEM.$TYPE.vcf.gz
+	fi
 
-    bgzip $FILTERSNP
-    tabix $FILTERSNP.gz
-fi
+	if [[ ! -f $FILTERSNP.gz || $STEM.$TYPE.vcf.gz -nt $FILTERSNP.gz ]]; then
+	    gatk VariantFiltration --output $FILTERSNP \
+		--variant $STEM.$TYPE.vcf.gz -R $REFGENOME \
+		--cluster-window-size 10  \
+		--filter-expression "QD < 2.0" --filter-name QualByDepth \
+		--filter-expression "MQ < 40.0" --filter-name MapQual \
+		--filter-expression "QUAL < 100" --filter-name QScore \
+		--filter-expression "SOR > 4.0" --filter-name StrandOddsRatio \
+		--filter-expression "FS > 60.0" --filter-name FisherStrandBias \
+		--missing-values-evaluate-as-failing --create-output-variant-index false
 
-if [[ ! -f $SELECTSNP.gz || $FILTERSNP.gz -nt $SELECTSNP.gz ]]; then
-    gatk SelectVariants -R $REFGENOME \
-	--variant $FILTERSNP.gz \
-	--output $SELECTSNP \
-	--exclude-filtered --create-output-variant-index false
-    bgzip $SELECTSNP
-    tabix $SELECTSNP.gz
-fi
+	#	--filter-expression "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	#	--filter-expression "ReadPosRankSum < -8.0" --filter-name ReadPosRank \
 
-TYPE=INDEL
-if [ ! -f $STEM.$TYPE.vcf.gz ]; then
-    gatk SelectVariants \
-        -R $REFGENOME \
-        --variant $GENOVCFOUT.gz \
-        -O $STEM.$TYPE.vcf  --select-type-to-include MIXED --select-type-to-include MNP \
-        --select-type-to-include $TYPE --create-output-variant-index false
-    bgzip $STEM.$TYPE.vcf
-    tabix $STEM.$TYPE.vcf.gz
-fi
+	    bgzip $FILTERSNP
+	    tabix $FILTERSNP.gz
+	fi
 
-if [[ ! -f $FILTERINDEL.gz || $STEM.$TYPE.vcf.gz -nt $FILTERINDEL.gz ]]; then
-    gatk VariantFiltration --output $FILTERINDEL \
-	--variant $STEM.$TYPE.vcf.gz -R $REFGENOME \
-	--cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
-	-filter "SOR > 10.0" --filter-name StrandOddsRatio \
-	-filter "FS > 200.0" --filter-name FisherStrandBias \
-	-filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
-	--create-output-variant-index false
+	if [[ ! -f $SELECTSNP.gz || $FILTERSNP.gz -nt $SELECTSNP.gz ]]; then
+	    gatk SelectVariants -R $REFGENOME \
+		--variant $FILTERSNP.gz \
+		--output $SELECTSNP \
+		--exclude-filtered --create-output-variant-index false
+	    bgzip $SELECTSNP
+	    tabix $SELECTSNP.gz
+	fi
 
-#	-filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank \
-#	-filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+	TYPE=INDEL
+	if [ ! -f $STEM.$TYPE.vcf.gz ]; then
+	    gatk SelectVariants \
+	        -R $REFGENOME \
+	        --variant $GENOVCFOUT.gz \
+	        -O $STEM.$TYPE.vcf  --select-type-to-include MIXED --select-type-to-include MNP \
+	        --select-type-to-include $TYPE --create-output-variant-index false
+	    bgzip $STEM.$TYPE.vcf
+	    tabix $STEM.$TYPE.vcf.gz
+	fi
 
-    bgzip $FILTERINDEL
-    tabix $FILTERINDEL.gz
-fi
+	if [[ ! -f $FILTERINDEL.gz || $STEM.$TYPE.vcf.gz -nt $FILTERINDEL.gz ]]; then
+	    gatk VariantFiltration --output $FILTERINDEL \
+		--variant $STEM.$TYPE.vcf.gz -R $REFGENOME \
+		--cluster-window-size 10  -filter "QD < 2.0" --filter-name QualByDepth \
+		-filter "SOR > 10.0" --filter-name StrandOddsRatio \
+		-filter "FS > 200.0" --filter-name FisherStrandBias \
+		-filter "InbreedingCoeff < -0.8" --filter-name InbreedCoef \
+		--create-output-variant-index false
 
-if [[ ! -f $SELECTINDEL.gz || $FILTERINDEL.gz -nt $SELETINDEL.gz ]]; then
-    gatk SelectVariants -R $REFGENOME \
-	--variant $FILTERINDEL.gz \
-	--output $SELECTINDEL \
-	--exclude-filtered --create-output-variant-index false
-    bgzip $SELECTINDEL
-    tabix $SELECTINDEL.gz
-fi
+	#	-filter "ReadPosRankSum < -20.0" --filter-name ReadPosRank \
+	#	-filter "MQRankSum < -12.5" --filter-name MapQualityRankSum \
+
+	    bgzip $FILTERINDEL
+	    tabix $FILTERINDEL.gz
+	fi
+
+	if [[ ! -f $SELECTINDEL.gz || $FILTERINDEL.gz -nt $SELECTINDEL.gz ]]; then
+	    gatk SelectVariants -R $REFGENOME \
+		--variant $FILTERINDEL.gz \
+		--output $SELECTINDEL \
+		--exclude-filtered --create-output-variant-index false
+	    bgzip $SELECTINDEL
+	    tabix $SELECTINDEL.gz
+	fi
+done
 
 if [ -d $TEMPDIR ]; then
 	rmdir $TEMPDIR
--- pipeline/06_make_SNP_tree.sh
+++ pipeline/06_make_SNP_tree.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/bash
-
-#SBATCH --mem=24gb --ntasks 32 --nodes 1
+#SBATCH --mem=24gb --ntasks 24 --nodes 1
 #SBATCH --time=2:00:00 -p short
 #SBATCH -J maketree --out logs/make_tree.log
 
+module load yq
 
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
- CPU=$SLURM_CPUS_ON_NODE
+  CPU=$SLURM_CPUS_ON_NODE
 fi
 
 if [[ -f config.txt ]]; then
-    source config.txt
+  source config.txt
 else
-    echo "Need a config.txt"
-    exit
+  echo "Need a config.txt"
+  exit
 fi
 
 if [[ -z $REFNAME ]]; then
-    REFNAME=REF
+  REFNAME=REF
 fi
+
+if [[ -z $POPYAML || ! -s $POPYAML ]]; then
+  echo "Cannot find \$POPYAML variable - set in config.txt"
+  exit
+fi
+
 module load parallel
 module load bcftools/1.11
 module load samtools/1.11
@@ -27,26 +33,48 @@ module load IQ-TREE/2.1.1
 module load fasttree
 
 print_fas() {
-    printf ">%s\n%s\n" $1 $(bcftools view -e 'AF=1' $2 | bcftools query -e 'INFO/AF < 0.1' -s $1 -f '[%TGT]')
+  printf ">%s\n%s\n" $1 $(bcftools view -e 'AF=1' $2 | bcftools query -e 'INFO/AF < 0.1' -s $1 -f '[%TGT]')
 }
 
-export -f print_fas
+iqtreerun() {
+	in=$1
+	out=$in.treefile
+	if [[ ! -f $out || $in -nt $out ]]; then
+		iqtree2 -m GTR+ASC -s $in -nt AUTO -b 100
+	fi
+}
+
+fasttreerun() {
+        in=$1
+	out=$(echo $in | perl -p -e 's/\.mfa/.fasttree.tre/')
+        if [[ ! -f $out || $in -nt $out ]]; then
+                FastTreeMP -gtr -gamma -nt < $in > $out
+        fi
+}
+
+export -f print_fas fasttreerun iqtreerun
 mkdir -p $TREEDIR
-for TYPE in SNP 
+for POPNAME in $(yq eval '.Populations | keys' $POPYAML | perl -p -e 's/^\s*\-\s*//')
 do
-    root=$FINALVCF/$PREFIX.$TYPE.combined_selected
-    FAS=$TREEDIR/$PREFIX.$TYPE.mfa
+  for TYPE in SNP
+  do
+    root=$FINALVCF/$PREFIX.$POPNAME.$TYPE.combined_selected
+    FAS=$TREEDIR/$PREFIX.$POPNAME.$TYPE.mfa
 
     if [ -f $root.vcf ]; then
-	bgzip $root.vcf
-	tabix $root.vcf.gz
+      bgzip $root.vcf
+      tabix $root.vcf.gz
     fi
     vcf=$root.vcf.gz
-    printf ">%s\n%s\n" $REFNAME $(bcftools view -e 'AF=1' ${vcf} | bcftools query -e 'INFO/AF < 0.1' -f '%REF') > $FAS
-    parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcf >> $FAS
-
-    perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*.]/-/g }' $FAS
-
-    FastTreeMP -gtr -gamma -nt < $FAS > $TREEDIR/$PREFIX.$TYPE.fasttree.tre
-    #iqtree2 -m GTR+ASC -s $FAS -nt 2
+    if [[ ! -f $FAS || ${vcf} -nt $FAS ]]; then
+      rm -f $FAS
+      # no ref genome alleles
+      #printf ">%s\n%s\n" $REFNAME $(bcftools view -e 'AF=1' ${vcf} | bcftools query -e 'INFO/AF < 0.1' -f '%REF') > $FAS
+      parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcf >> $FAS
+      perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*.]/-/g }' $FAS
+    fi
+  done
 done
+
+parallel -j 2 fasttreerun ::: $(ls $TREEDIR/*.mfa)
+parallel -j 4 iqtreerun ::: $(ls $TREEDIR/*.mfa)
