6e21bd3f8eb23af1d6f1045c7496b134f80525a2
Jason Stajich <jasonstajich.phd@gmail.com>
2021-02-24 12:20:38 -0800

    fixing for some unmapped reads target

--- pipeline/01_align.sh
+++ pipeline/01_align.sh
@@ -6,7 +6,7 @@ module load picard
 module load gatk/4
 module load java/13
 
-MEM=32g
+MEM=128g
 
 TMPOUTDIR=tmp
 
@@ -21,7 +21,7 @@ fi
 if [ ! -f $REFGENOME.dict ]; then
   echo "NEED a $REFGENOME.dict - make sure 00_index.sh is run"
 fi
-mkdir -p $TMPOUTDIR $ALNFOLDER
+mkdir -p $TMPOUTDIR $ALNFOLDER $UNMAPPED
 
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
@@ -45,50 +45,52 @@ fi
 IFS=,
 tail -n +2 $SAMPFILE | sed -n ${N}p | while read STRAIN FILEBASE
 do
-
-  # BEGIN THIS PART IS PROBABLY PROJECT SPECIFIC
-  # THIS COULD NEED TO BE CHANGED TO R1 R2 or R1_001 and R2_001 etc
-  PAIR1=$FASTQFOLDER/${FILEBASE}_1.$FASTQEXT
-  PAIR2=$FASTQFOLDER/${FILEBASE}_2.$FASTQEXT
   PREFIX=$STRAIN
-  # END THIS PART IS PROBABLY PROJECT SPECIFIC
-  echo "STRAIN is $STRAIN $PAIR1 $PAIR2"
-
-  TMPBAMFILE=$TEMP/$STRAIN.unsrt.bam
-  SRTED=$TMPOUTDIR/$STRAIN.srt.bam
-  DDFILE=$TMPOUTDIR/$STRAIN.DD.bam
   FINALFILE=$ALNFOLDER/$STRAIN.$HTCEXT
-
-  READGROUP="@RG\tID:$STRAIN\tSM:$STRAIN\tLB:$PREFIX\tPL:illumina\tCN:$RGCENTER"
-
+  echo "To process $PREFIX and $FINALFILE"
   if [ ! -s $FINALFILE ]; then
-    if [ ! -s $DDFILE ]; then
-      if [ ! -s $SRTED ]; then
-        if [ -e $PAIR1 ]; then
-          if [ ! -f $TMPBAMFILE ]; then
-            # potential switch this to bwa-mem2 for extra speed
-            bwa mem -t $CPU -R $READGROUP $REFGENOME $PAIR1 $PAIR2 | samtools view -1 -o $TMPBAMFILE
+    BAMSTOMERGE=()
+    for BASEPATTERN in $(echo $FILEBASE | perl -p -e 's/\;/,/g');
+    do
+      BASE=$(basename $BASEPATTERN | perl -p -e 's/(\S+)\[12\].+/$1/g; s/_R?$//g;')
+      # END THIS PART IS PROBABLY PROJECT SPECIFIC
+      echo "STRAIN is $STRAIN BASE is $BASE BASEPATTERN is $BASEPATTERN"
+
+      TMPBAMFILE=$TEMP/$BASE.unsrt.bam
+      SRTED=$TMPOUTDIR/$BASE.srt.bam
+      DDFILE=$TMPOUTDIR/$BASE.DD.bam
+
+      FINALFILE=$ALNFOLDER/$STRAIN.$HTCEXT
+      READGROUP="@RG\tID:$BASE\tSM:$STRAIN\tLB:$BASE\tPL:illumina\tCN:$RGCENTER"
+      echo "$TMPBAMFILE $READGROUP"
+#      echo "bwa mem -t $CPU -R $READGROUP $REFGENOME $FASTQFOLDER/$BASEPATTERN | samtools sort --threads $CPU -O bam -o $SRTED -T $TEMP -"
+
+      if [ ! -s $DDFILE ]; then
+        if [ ! -s $SRTED ]; then
+          if [ -e $PAIR1 ]; then
+            if [ ! -f $SRTED ]; then
+              # potential switch this to bwa-mem2 for extra speed
+              bwa mem -t $CPU -R $READGROUP $REFGENOME $FASTQFOLDER/$BASEPATTERN | samtools sort --threads $CPU -O bam -o $SRTED -T $TEMP -
+            fi
+          else
+            echo "Cannot find $BASEPATTERN, skipping $STRAIN"
+            exit
           fi
-        else
-          echo "Cannot find $PAIR1, skipping $STRAIN"
-          exit
-        fi
-        samtools fixmate --threads $CPU -O bam $TMPBAMFILE $TEMP/${STRAIN}.fixmate.bam
-        samtools sort --threads $CPU -O bam -o $SRTED -T $TEMP $TEMP/${STRAIN}.fixmate.bam
-        if [ -f $SRTED ]; then
-          rm -f $TEMP/${STRAIN}.fixmate.bam $TMPBAMFILE
-        fi
-      fi # SRTED file exists or was created by this block
+        fi # SRTED file exists or was created by this block
 
-      time java -jar $PICARD MarkDuplicates I=$SRTED O=$DDFILE \
-        METRICS_FILE=logs/$STRAIN.dedup.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT
-      if [ -f $DDFILE ]; then
-        rm -f $SRTED
-      fi
-    fi # DDFILE is created after this or already exists
-
-    samtools view -O $HTCFORMAT --threads $CPU --reference $REFGENOME -o $FINALFILE $DDFILE
-    samtools index $FINALFILE
+        time java -jar $PICARD MarkDuplicates I=$SRTED O=$DDFILE \
+          METRICS_FILE=logs/$STRAIN.dedup.metrics CREATE_INDEX=true VALIDATION_STRINGENCY=SILENT
+        if [ -f $DDFILE ]; then
+          rm -f $SRTED
+        fi
+      fi # DDFILE is created after this or already exists
+      BAMSTOMERGE+=( $DDFILE )
+    done
+    samtools merge --write-index -O $HTCFORMAT --threads $CPU --reference $REFGENOME $FINALFILE "${BAMSTOMERGE[@]}"
+    if [ -f $FINALFILE ]; then
+      rm -f "${BAMSTOMERGE[@]}"
+      rm -f $(echo "${BAMSTOMERGE[@]}" | sed 's/bam$/bai/')
+    fi
 
     if [ -f $FINALFILE ]; then
       rm -f $DDFILE
@@ -100,7 +102,7 @@ do
   UMAPSINGLE=$UNMAPPED/${STRAIN}_single.$FQ
   #echo "$UMAP $UMAPSINGLE $FQ"
 
-  if [ ! -f $UMAP ]; then
+  if [ ! -f $UMAP.gz ]; then
     module load BBMap
     samtools fastq -f 4 --threads $CPU -N -s $UMAPSINGLE -o $UMAP $FINALFILE
     pigz $UMAPSINGLE
