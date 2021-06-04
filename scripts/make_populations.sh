faf165051dc72ac0dd0bcc0c1aa03a5d263d4345
Jason Stajich <jasonstajich.phd@gmail.com>
2021-06-04 07:45:04 -0700

    rename output file

--- scripts/make_populations.sh
+++ scripts/make_populations.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash -l
 
-echo -e "Populations:\n\n  All:" > populations.yaml
+echo -e "Populations:\n\n  All:" > population_sets.yaml
 IFS=,
 while read POPNAME FILES
 do
 	echo "    - $POPNAME"
-done < samples.csv >> populations.yaml
+done < samples.csv >> population_sets.yaml
