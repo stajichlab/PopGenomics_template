bbbfa3e95ceef3e5ee991e6dff2ef4ea1b32dcaa
Jason Stajich <jasonstajich.phd@gmail.com>
2022-09-15 09:29:32 -0700

    use bcftools IUPAC generation of alleles for writing out alignment files for tree building

--- pipeline/06_make_SNP_tree.sh
+++ pipeline/06_make_SNP_tree.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/bash -l
-#SBATCH --mem=24gb --ntasks 48 --nodes 1 
+#!/usr/bin/bash
+#SBATCH --mem=24gb --ntasks 24 --nodes 1
 #SBATCH --time=2:00:00 -p short
 #SBATCH -J maketree --out logs/make_tree.log
 
 module load yq
-module load workspace/scratch
 
 CPU=2
 if [ $SLURM_CPUS_ON_NODE ]; then
   CPU=$SLURM_CPUS_ON_NODE
 fi
-MLTREECPU=6 # num of CPUs to request for IQTREE jobs
-FTTREECPU=32 # num of CPUs to request for Fasttree jobs
+
 if [[ -f config.txt ]]; then
   source config.txt
 else
@@ -28,21 +26,23 @@ if [[ -z $POPYAML || ! -s $POPYAML ]]; then
   exit
 fi
 
-module unload miniconda2
-module unload miniconda3
 module load parallel
 module load bcftools
 module load samtools
+module load iqtree
+module load fasttree
+module load workspace/scratch
 
 print_fas() {
-  printf ">%s\n%s\n" $1 $(bcftools query -s $1 -f '[%TGT]' $2)
+  printf ">%s\n%s\n" $1 $(bcftools query -s $1 -f '[%IUPACGT]' $2)
 }
+
 
 iqtreerun() {
 	in=$1
 	out=$in.treefile
 	if [[ ! -f $out || $in -nt $out ]]; then
-		sbatch -p intel -n $MLTREECPU -N 1 --mem 16gb -J iqtree --wrap "module load iqtree/2.2.0; iqtree2 -m GTR+ASC -s $in -nt AUTO -bb 1000 -alrt 1000"
+		sbatch -p intel -n 6 -N 1 --mem 16gb -J iqtree --wrap "module load IQ-TREE/2.1.1; iqtree2 -m GTR+ASC -s $in -st DNA -nt AUTO -bb 1000 -alrt 1000"
 	fi
 }
 
@@ -50,7 +50,7 @@ fasttreerun() {
         in=$1
 	out=$(echo $in | perl -p -e 's/\.mfa/.fasttree.tre/')
         if [[ ! -f $out || $in -nt $out ]]; then
-                sbatch -p short -n $FTTREECPU -N 1 --mem 24gb -p short -J FastTree --wrap "module load fasttree; FastTreeMP -gtr -gamma -nt < $in > $out"
+                sbatch -p short -n 32 -N 1 --mem 16gb -p short -J FastTree --wrap "module load fasttree; FastTreeMP -gtr -gamma -nt < $in > $out"
         fi
 }
 
@@ -70,19 +70,15 @@ do
 
     vcf=$root.vcf.gz
     if [[ ! -f $FAS || ${vcf} -nt $FAS ]]; then
-      rm -f $FAS
-      vcftmp=$SCRATCH/$PREFIX.$POPNAME.$TYPE.combined_selected.bcf
-      bcftools view --threads 4 -e 'QUAL < 1000 || AF=1 || INFO/AF < 0.1' $vcf -Ob -o $vcftmp 
-      bcftools index $vcftmp
-
-      #rsync -a $vcf.tbi $vcftmp.tbi
+      vcftmp=$SCRATCH/$PREFIX.$POPNAME.$TYPE.vcf.gz
+      bcftools filter -O z -o $vcftemp --SnpGap 3 -e 'QUAL < 1000 || AF=1 || INFO/AF < 0.05 || F_MISSING > 0' $vcf
+      bcftools index $vcftemp
       # no ref genome alleles
-      printf ">%s\n%s\n" $REFNAME $(bcftools query -f '%REF' ${vcftmp}) > $FAS
+      printf ">%s\n%s\n" $REFNAME $(bcftools query -f '%REF') > $FAS
       parallel -j $CPU print_fas ::: $(bcftools query -l ${vcf}) ::: $vcftmp >> $FAS
-      perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*.]/-/g }' $FAS
+#      perl -ip -e 'if(/^>/){s/[\(\)#]/_/g; s/_+/_/g } else {s/[\*.]/-/g }' $FAS
     fi
   done
 done
-exit
-parallel -j 2 fasttreerun ::: $(ls $TREEDIR/*.mfa)
-parallel -j 4 iqtreerun ::: $(ls $TREEDIR/*.mfa)
+#parallel -j 2 fasttreerun ::: $(ls $TREEDIR/*.mfa)
+#parallel -j 4 iqtreerun ::: $(ls $TREEDIR/*.mfa)
