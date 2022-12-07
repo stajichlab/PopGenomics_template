528d67c7e1ca139321cf36ade47e01c656e9c58d
Jason Stajich <jasonstajich.phd@gmail.com>
2022-12-07 09:24:20 -0800

    generate coverage values with mosdepth

--- pipeline/11_mosdepth.sh
+++ pipeline/11_mosdepth.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/bash -l
+#!/usr/bin/bash
 #SBATCH --nodes 1 --ntasks 24 --time 2:00:00 -p short --mem 64G --out logs/mosdepth.parallel.log
-#SBATCH -J modepth
 
+#SBATCH -J modepth
 # This one goes to 11
 module load parallel
-
 CPU=$SLURM_CPUS_ON_NODE
-if [ -z $CPU ]; then
+if [ ! $CPU ]; then
  CPU=2
 fi
 module load mosdepth
